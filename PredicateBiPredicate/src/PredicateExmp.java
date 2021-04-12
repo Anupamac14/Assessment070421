11303750291d897401a3b3b0e5184faa13dae71a
AnuV2C18467 <AnuV2C18467@V2CP0081.vee2office.com>
2021-04-12 11:50:00 +0530

    Predicate Bi-Predicate

---
+++ PredicateBiPredicate/src/PredicateExmp.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PredicateExmp {
+	public static void main(String[] args) {
+		ArrayList<Student> al = new ArrayList<Student>();
+		al.add(new Student(101, "Anu", 21));
+		al.add(new Student(103, "Racchu", 13));
+		al.add(new Student(102, "Buff", 18));
+		al.add(new Student(105, "Netty", 24));
+		al.add(new Student(104, "Meghu", 15));
+		Predicate<Student> p1 = i -> (i.age >= 18);
+		Predicate<Student> p2 = e -> (e.age < 18);
+		for (Student s : al) {
+			if (p1.test(s)) {
+
+				System.out.println("Student Age is :" + s.age + " " + "Student Name is:" + s.name);
+				System.out.println("Allowed to watch movie");
+				System.out.println(" ");
+			}
+
+			else if (p2.test(s)) {
+
+				System.out.println("Student Age is :" + s.age + " " + "Student Name is:" + s.name);
+				System.out.println("Not Allowed to watch movie");
+				System.out.println("  ");
+			}
+
+		}
+	}
+}
+
+class Student {
+	int id;
+	String name;
+	int age;
+
+	public Student(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+}
+
